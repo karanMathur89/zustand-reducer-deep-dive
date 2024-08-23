b8e844097a23d246500e64776d52b88ff88ef59b
karanMathur89 <74524108+karanMathur89@users.noreply.github.com>
2024-08-23 11:40:07 +0530

    reafactor: changed Output markup

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -7,12 +7,11 @@ export default function Output() {
   return (
     <section className="grid gap-2 rounded border border-blue-300 bg-blue-100 p-2">
       <p className="text-xl font-semibold">Output</p>
-      <p className="border-b border-dashed border-blue-300 pb-2 text-gray-600">
+      <p className="border-b border-dashed border-blue-300 pb-2 text-sm text-gray-600">
         This is a client component in the main layout
       </p>
-      <pre className="mt-4 font-bold">Count: {count}</pre>
-      <pre className="mt-4 font-bold">
-        isTyping: {isTyping ? "true" : "false"}
+      <pre className="mt-4 text-xl font-semibold">
+        {JSON.stringify({ count, isTyping }, null, 2)}
       </pre>
     </section>
   )
